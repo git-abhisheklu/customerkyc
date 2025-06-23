c8b5136f6ee82a64278c5d6fbab4341da1e126a4
abhishek gupta <abhishekgupta.lu@gmail.com>
2025-06-23 16:19:41 +0530

    initial commit

---
+++ customerkyc/src/main/java/com/demopoc/customerkyc/CustomerkycApplication.java
@@ -0,0 +1,13 @@
+package com.demopoc.customerkyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerkycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerkycApplication.class, args);
+	}
+
+}
---
+++ customerkyc/src/test/java/com/demopoc/customerkyc/CustomerkycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demopoc.customerkyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerkycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
